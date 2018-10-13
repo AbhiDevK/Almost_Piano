abedc617487436682557531c16b2c6eec090f37f
Abhishek <abhilokare321@gmail.com>
2018-10-13 23:04:31 +0530

    Piano working completed

---
+++ app.js
@@ -0,0 +1,20 @@
+window.addEventListener("keydown", e => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+});
+
+function removeTransform(e){
+if(e.propertyName !== "transform") return;
+this.classList.remove('playing')
+}
+//select all keys
+const keys = document.querySelectorAll('.key')
+console.log(keys)
+keys.forEach((key)=>{
+    key.addEventListener('transitionend', removeTransform)
+})
